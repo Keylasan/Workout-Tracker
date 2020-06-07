d6c0b70491ca52804134b4ac39b52f325997fbe6
Life is Now <keylasan28@yahoo.com>
2020-06-07 17:03:06 -0400

    redone

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
+// Dependencies
 const express = require("express");
-const mongoose = require("mongoose");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 8889;
+// Setting up Express App
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(morgan("dev"));
+
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-trackerDB", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
-// routes
-require("./routes/api")(app);
-require("./routes/html")(app);
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.use(express.static('public'));
+
+// db mongo
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+})
+
+// Creating Routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function(){
+    console.log(`App listening on Port ${PORT}!`);
 });
